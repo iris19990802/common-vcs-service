854c308567e5aba3c72e7d7025758ea26b91ac16
Iris <50256455+iris19990802@users.noreply.github.com>
2020-05-17 20:48:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,3 @@
-# 切换到工作目录
-cd /vcs/work_dir/
-
-# 拷贝 jar包(容器外) 到 work_dir（容器内）
-COPY ./target/os-version-1.0-SNAPSHOT.jar  /vcs/work_dir
-
 # 运行java服务 (后台运行)
 java -jar os-version-1.0-SNAPSHOT.jar &
 
